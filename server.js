b74db041a912c965155bc422ce786510e3d743e0
Nikita Novik <nikita.a.novik@gmail.com>
2020-01-14 11:22:38 -0700

    final version

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [{weather:'Weather is good!', condition:'I feel good!'}]; //an endpoint with a dummy entry.
 
 // Require Express to run server and routes
 var express = require('express');
 var fetch = require('node-fetch');
-var router = express.Router();
 
 // Start up an instance of app
 var app = express();
@@ -24,7 +23,7 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = 8001;
+const port = 8002;
 
 const server = app.listen(port, listening);
 
@@ -33,44 +32,21 @@ function listening() {
     console.log(`running on localhost: ${port}`);
 }
 
-router.get('/all', function(req, res) {
-    res.send('hello world');
-})
-
-module.exports = router;
-
-
-// var wiki = require('/wiki.js');
-
-const entries = [{weather:'Weather is good!', condition:'I feel good!'}];
-
-
-
 const apiKey = '12a6896397a5e52cb5eb4ab069607559';
-
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
-
-// fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${apiKey}`)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((myJson) => {
-//       console.log(myJson);
-//     });
-
-
 
 async function fetchWeatherAppDataByZip(zip) {
     const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${apiKey}`);
-    return data.json();
+    const json = await data.json();
+    const description = json.list[0].weather[0].description;
+    console.log(description);
+    return description;
 }
 
-// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}
-
-async function addToEntries(zip, message) {
+async function addToProjectData(zip, message) {
     const weather = await fetchWeatherAppDataByZip(zip);
     const entry = {weather, message};
-    entries.push(entry);
+    projectData.push(entry);
+    console.log(projectData);
 }
 
 function getParams(req){
@@ -78,9 +54,9 @@ function getParams(req){
     return req.body;
 };
 
-app.use('/entry', (req, res) => {
+app.use('/entry', async (req, res) => {
     const parameters = getParams(req);
-    addToEntries(parameters.zip, parameters.message);
+    await addToProjectData(parameters.zip, parameters.message);
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(entries[entries.length-1]));
+    res.end(JSON.stringify(projectData[projectData.length-1]));
 } );
