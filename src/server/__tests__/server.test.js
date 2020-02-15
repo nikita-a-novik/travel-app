e82de65e81218e181f987df02c8f15867e3b3351
Nikita Novik <nikita.a.novik@gmail.com>
2020-02-15 16:08:20 -0700

    Refactor

--- src/server/__tests__/server.test.js
+++ src/server/__tests__/server.test.js
@@ -1,79 +1,17 @@
 const f = require('node-fetch')
 
-const getRawGeoDataByQuery = async (location, fetch) => {
-  const base = 'api.geonames.org'
-  const username = 'username=nikitanovik'
-  const type = 'type=json'
-  const query = `q=${location}`
-  const url = `http://${base}/search?${username}&${type}&${query}`
-  try {
-    return await fetch(url).then(res => res.json())
-  } catch (e) {
-    return {}
-  }
-}
-
-const getGeoData = (rawGeoData) => {
-  const {
-    geonames: [
-      {
-        lng = 0,
-        lat = 0,
-        countryName = ''
-      } = {}
-    ] = []
-  } = rawGeoData || {}
-  return {
-    longitude: lng,
-    latitude: lat,
-    country: countryName
-  }
-}
-
-const getUnixTimestamp = (date) => {
-  try {
-    return Math.round(date.getTime() / 1000);
-  } catch (e) {
-    return 0
-  }
-}
-
-const getRawFutureWeatherData = async (geoData, date, fetch) => {
-  const timestamp = getUnixTimestamp(date)
-  const { longitude = 0, latitude = 0 } = geoData || {}
-  const base = 'api.darksky.net'
-  const key = '23944bc9ab2bd4c11b082c0bc9895c0f'
-  const url = `https://${base}/forecast/${key}/${latitude},${longitude},${timestamp}`
-  try {
-    return await fetch(url).then(res => res.json())
-  } catch (e) {
-    return {}
-  }
-}
-
-const getFutureWeatherDataForLocation = async (location) => {
-  const rawGeoData = await getRawGeoDataByQuery(location)
-  const geoData = getGeoData(rawGeoData)
-  const rawFutureWeatherData = getRawFutureWeatherData(geoData)
-  const processedFutureWeatherData = getFutureWeatherData(rawFutureWeatherData)
-  return processedFutureWeatherData
-}
-
-const getFutureWeatherData = (weatherData) => {
-  const {
-    currently: {
-      summary = ''
-    } = {}
-  } = weatherData || {};
-  return summary
-}
+const {
+  getFutureWeatherDataForLocation,
+  getGeoData,
+  getRawGeoDataByQuery,
+  getUnixTimestamp,
+  getRawFutureWeatherData,
+  getFutureWeatherData
+} = require('../server');
 
 describe('Server', () => {
-  it.only('should get geographic information based on city', async (done) => {
-    const geoData = getGeoData(await getRawGeoDataByQuery('Boulder', f))
-    const date = new Date('02/16/2020')
-    const weatherData = await getRawFutureWeatherData(geoData, date, f);
-    const processedFutureWeatherData = getFutureWeatherData(weatherData);
+  it.skip('should get geographic information based on city', async (done) => {
+    const processedFutureWeatherData = await getFutureWeatherDataForLocation('Boulder', '02/15/2020', f);
     console.warn(processedFutureWeatherData)
     done()
   })
--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,77 @@
-import f from 'node-fetch'
-
-const getRawGeoDataByQuery = async (location) => {
-    const base = 'api.geonames.org'
-    const username = 'username=nikitanovik'
-    const type = 'type=json'
-    const query = `q=${location}`
-    const url = `http://${base}/search?${username}&${type}&${query}`
-    return f(url).then(res => res.json())
+const getRawGeoDataByQuery = async (location, fetch) => {
+  const base = 'api.geonames.org'
+  const username = 'username=nikitanovik'
+  const type = 'type=json'
+  const query = `q=${location}`
+  const url = `http://${base}/search?${username}&${type}&${query}`
+  try {
+    return await fetch(url).then(res => res.json())
+  } catch (e) {
+    return {}
+  }
 }
 
-const getFutureWeatherDataForLocation = async (location) => {
-    const rawGeoData = await getRawGeoDataByQuery(location)
-    const geoData = getGeoData(rawGeoData)
-    const rawFutureWeatherData = getRawFutureWeatherData(geoData)
-    const processedFutureWeatherData = getFutureWeatherData(rawFutureWeatherData)
-    return processedFutureWeatherData
+const getGeoData = (rawGeoData) => {
+  const {
+    geonames: [
+      {
+        lng = 0,
+        lat = 0,
+        countryName = ''
+      } = {}
+    ] = []
+  } = rawGeoData || {}
+  return {
+    longitude: lng,
+    latitude: lat,
+    country: countryName
+  }
+}
+
+const getUnixTimestamp = (date) => {
+  try {
+    return Math.round(date.getTime() / 1000);
+  } catch (e) {
+    return 0
+  }
+}
+
+const getRawFutureWeatherData = async (geoData, date, fetch) => {
+  const timestamp = getUnixTimestamp(date)
+  const { longitude = 0, latitude = 0 } = geoData || {}
+  const base = 'api.darksky.net'
+  const key = '23944bc9ab2bd4c11b082c0bc9895c0f'
+  const url = `https://${base}/forecast/${key}/${latitude},${longitude},${timestamp}`
+  try {
+    return await fetch(url).then(res => res.json())
+  } catch (e) {
+    return {}
+  }
+}
+
+const getFutureWeatherDataForLocation = async (location, dateString, fetch) => {
+  const date = new Date(dateString)
+  const rawGeoData = await getRawGeoDataByQuery(location, fetch)
+  const geoData = getGeoData(rawGeoData)
+  const rawFutureWeatherData = await getRawFutureWeatherData(geoData, date, fetch)
+  const processedFutureWeatherData = getFutureWeatherData(rawFutureWeatherData)
+  return processedFutureWeatherData
+}
+
+const getFutureWeatherData = (weatherData) => {
+  const {
+    currently: {
+      summary = ''
+    } = {}
+  } = weatherData || {};
+  return summary
+}
+
+module.exports = {
+  getFutureWeatherData,
+  getFutureWeatherDataForLocation,
+  getGeoData,
+  getRawGeoDataByQuery,
+  getRawFutureWeatherData,
+  getUnixTimestamp,
 }
